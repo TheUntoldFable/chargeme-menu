415152ab61fa13a1322c6a5d4718a890f498f39b
uzun0ff <tsvetomir.uzunov@looming.tech>
2024-07-12 15:51:22 +0300

    Tries to fix production deployment with config updates.

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,11 @@
 
 import { withHydrationOverlay } from "@builder.io/react-hydration-overlay/next"
 
+const isProd = process.env.NODE_ENV === "production"
+
 const nextConfig = {}
 
-export default withHydrationOverlay({
+export default isProd ? nextConfig : withHydrationOverlay({
   /**
    * Optional: `appRootSelector` is the selector for the root element of your app. By default, it is `#__next` which works
    * for Next.js apps with pages directory. If you are using the app directory, you should change this to `main`.
